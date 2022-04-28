f7632b4d0cc9949bfb31ade730e3eead1029a9e1
Giiani <gianipennisi@gmail.com>
2022-04-28 15:16:38 -0300

    succes

--- Fantasy Game Inventory.py
+++ Fantasy Game Inventory.py
@@ -7,6 +7,18 @@ def displayInventory(items):
     
     print("Total number of items:"+str(total))
     
-items={'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
-
+def addToInventory(inventory, addedItems):
+    for i in range(len(addedItems)):
+        if addedItems[i] in inventory:
+            inventory[addedItems[i]]+=1
+        else:
+            inventory.setdefault(addedItems[i],1)
+    
+    return inventory
+            
+            
+items = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+addedItems = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+displayInventory(items)
+items = addToInventory(items,addedItems)
 displayInventory(items)
